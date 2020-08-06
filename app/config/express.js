ae786fedff73e257a5fc16da297fa8bd9608ba29
dkingstong <d.kingston12@gmail.com>
2020-08-05 23:45:33 -0500

    Define document routes (#3)

--- app/config/express.js
+++ app/config/express.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+//import routes from '../routes'
 export default function () {
     const app = express();
     app.use(bodyParser.json());
+    //app.use(routes);
     return app;
 }

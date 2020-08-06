41adc621f1cb4310744e7a7af1fbf854e2e05d27
Dennis Kingston <d.kingston12@gmail.com>
2020-08-06 00:13:15 -0500

    Add controller and missing routes file

--- app/config/express.js
+++ app/config/express.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-//import routes from '../routes'
+import routes from '../routes'
+
 export default function () {
     const app = express();
     app.use(bodyParser.json());
-    //app.use(routes);
+    app.use(routes);
     return app;
 }

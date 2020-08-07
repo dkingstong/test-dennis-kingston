5d0d17cefc4235e845ef92e7530c3ef7deed7591
Dennis Kingston <d.kingston12@gmail.com>
2020-08-06 22:31:57 -0500

    Add versions table

---
+++ app/migrations/20200807032916-create-versioned-table.js
@@ -0,0 +1,49 @@
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('document', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+        type: Sequelize.BIGINT,
+      },
+      documentId: {
+        allowNull: false,
+        type: DataTypes.BIGINT,
+        references: {
+          model: 'document',
+          key: 'id',
+        },
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      url: {
+        type: Sequelize.STRING,
+      },
+      content: {
+        type: Sequelize.TEXT,
+      },
+      category: {
+        type: Sequelize.ENUM,
+        values: ['economics', 'political', 'social'],
+      },
+      version: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface) => {
+    await queryInterface.dropTable('document');
+  },
+};

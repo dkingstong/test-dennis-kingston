4e9f6048a1cf5e5048077dc63bca8317d414dd9a
Dennis Kingston <d.kingston12@gmail.com>
2020-08-06 22:42:29 -0500

    Fix typo

--- app/migrations/20200807032916-create-versioned-table.js
+++ app/migrations/20200807032916-create-versioned-table.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('document', {
+    await queryInterface.createTable('versioned_doc', {
       id: {
         allowNull: false,
         primaryKey: true,

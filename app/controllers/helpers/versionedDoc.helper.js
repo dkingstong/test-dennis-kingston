261e694d87f86b632cf7c75dd1a461bfd6333c52
Dennis Kingston <d.kingston12@gmail.com>
2020-08-07 02:39:24 -0500

    Fix versioning

--- app/controllers/helpers/versionedDoc.helper.js
+++ app/controllers/helpers/versionedDoc.helper.js
@@ -1,5 +1,4 @@
 export function docToVersionedDoc(document) {
-  document.version = document.version + 1;
   const { id, created_at, updated_at, ...versionedDoc } = document.dataValues;
   return {
     documentId: document.id,
